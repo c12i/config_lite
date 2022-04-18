94f5a24feb9996798998211f177db44c8a0bea9b
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-04-19 00:28:24 +0300

    Implement get value from env var in parse yaml function

--- src/parser/yaml.rs
+++ src/parser/yaml.rs
@@ -1,6 +1,8 @@
 use crate::{ConfigError, ConfigResult};
 
-pub fn parse_yaml<'a, T: for<'de> serde::Deserialize<'de>>(
+use super::get_value_from_env_var;
+
+pub(crate) fn parse_yaml<'a, T: for<'de> serde::Deserialize<'de>>(
     file_content: &str,
     string_path: &str,
 ) -> ConfigResult<T> {
@@ -11,6 +13,11 @@ pub fn parse_yaml<'a, T: for<'de> serde::Deserialize<'de>>(
         current_value = current_value.get(s).ok_or_else(|| {
             ConfigError::ValueError(current_value.as_str().unwrap_or("").to_string())
         })?;
+    }
+    if current_value.is_string() {
+        if let Some(value_string) = current_value.as_str() {
+           return get_value_from_env_var(value_string);
+        }
     }
     Ok(serde_yaml::from_value(current_value.to_owned())?)
 }
